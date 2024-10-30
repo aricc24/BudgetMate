eeb9777b50a7e8ba327c7ea685d5ce9770471bd4
aricc24 <aricc24@ciencias.unam.mx>
2024-10-30 14:14:12 -0600

    fix: dependencies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,6 +51,18 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+#DATABASES = {
+    #'default': {
+      #  'ENGINE': 'django.db.backends.mysql',
+      #  'NAME': os.getenv('DB_NAME'),
+      #  'USER': os.getenv('DB_USER'),
+      #  'PASSWORD': os.getenv('DB_PASSWORD'),
+      #  'HOST': os.getenv('DB_HOST', 'db'),
+      #  'PORT': os.getenv('DB_PORT', '3306'),
+    #}
+#}
+
+
 ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
