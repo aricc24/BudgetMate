0c958ece9dfae1d5b63e57d5f38eb3d289ae1d7a
CervantesSanti2005 <SantiagoCervantes@ciencias.unam.mx>
2024-11-02 17:55:12 -0600

    Pagina de login en react y css no dinámica

---
+++ budgetmate/src/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Username:', username);
+        console.log('Password:', password);
+
+    };
+
+    return(
+        <div className= "login-container">
+         <form onSubmit={handleSubmit} className="login-form">
+           <h2>Login</h2>
+           <div className="inputGroup">
+            <label htmlFor="username">Usuario:</label>
+            <input
+             type="text"
+             id ="username"
+             value={username}
+             onChange={(e) => setUsername(e.target.value)}
+             placeholder="Usuario"
+             required
+            />
+          </div>
+          <div className="input-group">
+           <label htmlFor="password">Contraseña:</label>
+           <input
+             type="password"
+             id="password"
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+             placeholder="Contraseña"
+             required
+           />
+         </div>
+         <button type="submit" className="login-button">Login</button>
+        </form>
+       </div>
+
+    );
+}
+
+export default Login;
