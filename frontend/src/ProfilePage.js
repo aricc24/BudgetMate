10e1a549b344e90ece409e17f4ae044e02d856b6
aricc24 <aricc24@ciencias.unam.mx>
2024-11-07 17:51:27 -0600

    feat: docker

--- frontend/src/ProfilePage.js
+++ frontend/src/ProfilePage.js
@@ -6,12 +6,12 @@ function ProfilePage() {
     name: '',
     lastName: '',
     rfc: '',
-    bio: '',
+    CURP: '',
     phoneNumber: '',
   });
 
   useEffect(() => {
-    setProfileData({ name: 'John', lastName: 'Doe', rfc: 'RFC1234', bio: 'My Bio', phoneNumber: '1234567890' });
+    setProfileData({ name: 'Nombre', lastName: 'Apellido', rfc: 'RFC1234', CURP: 'CURP', phoneNumber: '1234567890' });
   }, []);
 
   const handleChange = (e) => {
@@ -25,12 +25,12 @@ function ProfilePage() {
 
   return (
     <div className="profile-page">
-      <h2>Edit Profile</h2>
+      <h2>My Profile</h2>
       <form>
         <input name="name" value={profileData.name} onChange={handleChange} placeholder="Name" />
         <input name="lastName" value={profileData.lastName} onChange={handleChange} placeholder="Last Name" />
         <input name="rfc" value={profileData.rfc} onChange={handleChange} placeholder="RFC" />
-        <textarea name="bio" value={profileData.bio} onChange={handleChange} placeholder="Bio"></textarea>
+        <textarea name="CURP" value={profileData.CURP} onChange={handleChange} placeholder="CURP"></textarea>
         <input name="phoneNumber" value={profileData.phoneNumber} onChange={handleChange} placeholder="Phone Number" />
         <button type="button" onClick={saveChanges}>Save Changes</button>
       </form>
