52dd0c93b77f551020d65a61d8b267c2ebdaaf11
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-11-07 20:12:46 -0600

    fix: refact docker settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.mysql',
        'NAME': os.getenv('DB_NAME'),
-       'USER': os.getenv('DB_USER'),
-       'PASSWORD': os.getenv('DB_PASSWORD'),
+       'USER': 'root',
+       'PASSWORD': os.getenv('DB_ROOT_PASSWORD'),
        'HOST': os.getenv('DB_HOST', 'db'),
        'PORT': os.getenv('DB_PORT', '3306'),
     }
