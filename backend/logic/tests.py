f148332bd869dc3ccca74488cf4271876856f7c4
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-11-11 21:43:40 -0600

    test: Add 4 unit test 	- test_login: checks the cases where the user inputs correct data and incorrect data in login. 	- test_get_user: checks if the api give the correct data. 	- test_create_transaction: checks if the creation transactions is correct. 	- test_update_transaction: checks if the api updates correctly the transaction data.

--- backend/logic/tests.py
+++ backend/logic/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import *
 
-# Create your tests here.
+class APITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(
+            email="testuser@gmail.com",
+            password="123password",
+            first_name="User",
+            last_name_father="Test",
+            curp="TESTU123456789",
+            rfc="TESTRFC1234"
+        )
+        self.transaction_url = reverse('transactions-list')
+        self.login_url = reverse('user-log')
+        self.user_info_url = reverse('user-info')
+
+    def test_login(self):
+        data = {
+            "email": "testuser@gmail.com",
+            "password": "123password"
+        }
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("Logged in successfully!", response.data["message"])
+        data = {
+            "email": "user_incorrect@gmail.com",
+            "password": "wrongpassword"
+        }
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertIn("Sorry, invalid data", response.data["message"])
+
+    def test_get_user(self):
+        data = {"id": self.user.id_user}
+        response = self.client.post(self.user_info_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["email"], "testuser@gmail.com")
+
+    def test_create_transaction(self):
+        data = {
+            "id_user": self.user.id_user,
+            "mount": 100.0,
+            "type": Transaction.TransEnum.EXPENSE
+        }
+        response = self.client.post(self.transaction_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["mount"], 100.0)
+        self.assertEqual(response.data["type"], Transaction.TransEnum.EXPENSE)
+
+    def test_update_transaction(self):
+        transaction = Transaction.objects.create(
+            id_user= self.user,
+            mount= 100.14,
+            type= Transaction.TransEnum.EXPENSE
+        )
+        update_url = reverse('transaction-update', kwargs={
+            'id_user': self.user.id_user,
+            'id_transaction': transaction.id_transaction
+        })
+
+        data = {
+            "id_user": self.user.id_user,
+            "mount": 1502.14,
+            "type": Transaction.TransEnum.INCOME
+        }
+        response = self.client.put(update_url, data, format='json')
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["mount"], 1502.14)
+        self.assertEqual(response.data["type"], Transaction.TransEnum.INCOME)
