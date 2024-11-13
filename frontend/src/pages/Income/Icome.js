339c30e101947a3b4ee760a8b33638a4153ff822
aricc24 <aricc24@ciencias.unam.mx>
2024-11-13 17:12:41 -0600

    feat: the logic of the requests the backend to show the user's income, handling the transactions as arrangements

--- frontend/src/pages/Income/Icome.js
+++ frontend/src/pages/Income/Icome.js
@@ -12,21 +12,18 @@ const Income = () => {
     useEffect(() => {
         const fetchTransactions = async () => {
             const authToken = localStorage.getItem('authToken');
-            const userId = localStorage.getItem('userId');
+            if (!authToken) {
+                navigate('/login');
+                return;
+            }
 
-            //if (!authToken || !userId) {
-                //navigate('/login');
-                //return;
-           // }
-
-            const response = await fetch(`http://127.0.0.1:8000/api/user/${userId}/transactions/`, {
+            const response = await fetch('http://127.0.0.1:8000/api/transactions/', {
                 headers: { 'Authorization': `Bearer ${authToken}` }
             });
             const data = await response.json();
-            const incomeTransactions = data.filter(t => t.type === 0); 
+            const incomeTransactions = data.filter(t => t.type === 0); // Filtramos solo ingresos
             setTransactions(incomeTransactions);
             updateChartData(incomeTransactions);
-
         };
         
         fetchTransactions();
