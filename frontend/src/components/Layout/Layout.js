3fc4ab07069e35b9fa567665e1213990f7a20416
aricc24 <aricc24@ciencias.unam.mx>
2024-11-15 13:45:24 -0600

    feat: the layout placed as a general component to avoid problems with home but now we have to solve problems with logIn

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import Home from '../../pages/Home/Home';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import SidebarHamburger from '../../components/SidebarHamburger/SidebarHamburger';
+import ProfileIcon from '../../components/ProfileIcon/ProfileIcon';
+import './Layout.css';
 
 const Layout = ({ children }) => {
-    return (
-        <div className="layout">
-            <Home />
-            <div className="layout-content">
-                {children} 
-            </div>
-        </div>
-    );
+  const navigate = useNavigate();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarHamburgerOpen, setIsSidebarHamburgerOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const toggleSidebarHamburger = () => {
+    setIsSidebarHamburgerOpen(!isSidebarHamburgerOpen);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('authToken');
+    navigate('/login');
+  };
+
+  return (
+    <div className="layout">
+      <nav className="home-navbar">
+        <button className="menu-icon" onClick={toggleSidebarHamburger}>
+          <i className="fas fa-bars"></i>
+        </button>
+        
+        <ProfileIcon onClick={toggleSidebar} />
+      </nav>
+
+      {isSidebarHamburgerOpen && <SidebarHamburger />}
+
+      {isSidebarOpen && <Sidebar toggleSidebar={toggleSidebar} logout={logout} />}
+      
+      <div className="layout-content" style={{ paddingTop: '120px' }}>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
