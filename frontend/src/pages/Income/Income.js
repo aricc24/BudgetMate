19e6edf42f4b2990f9676c92bc1a184e172bdc9c
aricc24 <aricc24@ciencias.unam.mx>
2024-11-29 18:34:01 -0600

    feat: the function was created in the frontend to manage the sending of the request to generate the PDF

--- frontend/src/pages/Income/Income.js
+++ frontend/src/pages/Income/Income.js
@@ -286,7 +286,32 @@ const Income = () => {
         } catch (error) {
             console.error('Error updating category:', error);
         }
-    };
+    }; 
+
+    const handleDownloadPDF = async () => {
+        const authToken = localStorage.getItem('authToken');
+        const userId = localStorage.getItem('userId');
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/api/generate_pdf/${userId}/`, {
+                headers: { 'Authorization': `Bearer ${authToken}` }
+            });
+            if (response.ok) {
+                const blob = await response.blob();
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = `report_${userId}.pdf`;
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            } else {
+                console.error('Failed to generate PDF');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+     };
+     
     
 
     return (
@@ -302,6 +327,9 @@ const Income = () => {
                         <option value="yearly">Yearly</option>
                     </select>
                 </div> */}
+                
+                <button onClick={handleDownloadPDF}>Downlad PDF</button>
+
 
                 <div className="add-income-form">
                     <input
@@ -549,6 +577,8 @@ const Income = () => {
         </Layout>
     );
 };
+
+
 
 
 
