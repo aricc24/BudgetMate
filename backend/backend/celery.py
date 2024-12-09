f13ab3400ab84a8a25a18121e1e8d217efe9c376
aricc24 <aricc24@ciencias.unam.mx>
2024-12-08 22:59:23 -0600

    fix: celery configuration

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,13 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 app = Celery('backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.beat_schedule = {
+    'process-scheduled-transactions-every-minute': {
+        'task': 'logic.tasks.process_scheduled_transactions',
+        'schedule': crontab(minute='*/1'),  # Ejecutar cada minuto
+    },
+}
+
 app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
