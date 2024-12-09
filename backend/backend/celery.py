de551620b802d5f50dfbf74f9967c48d25090b2b
aricc24 <aricc24@ciencias.unam.mx>
2024-12-08 22:47:30 -0600

    fix: docker configuration

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -5,9 +5,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 app = Celery('backend')
-
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
 app.autodiscover_tasks()
 
 @app.task(bind=True)
