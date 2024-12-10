5064cfc59479196dfa749f0a39d1e8526a2cdcdc
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-12-10 16:05:47 -0600

    History page was divided in two files: History.jsx file where is all history logic. HistoryComponents.jsx file where is history components

---
+++ frontend/src/pages/History/History.jsx
@@ -0,0 +1,206 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import HistoryComponents from './HistoryComponents';
+
+const History = () => {
+    const [isCategoryDialogOpen, setIsCategoryDialogOpen] = useState(false);
+    const [transactions, setTransactions] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [minAmount, setMinAmount] = useState('');
+    const [maxAmount, setMaxAmount] = useState('');
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [isCategoryIncomeDialogOpen, setIsCategoryIncomeDialogOpen] = useState(false);
+    const [incomes, setIncomes] = useState([]);
+    const [startDateIncomes, setStartDateIncomes] = useState('');
+    const [endDateIncomes, setEndDateIncomes] = useState('');
+    const [minAmountIncomes, setMinAmountIncomes] = useState('');
+    const [maxAmountIncomes, setMaxAmountIncomes] = useState('');
+    const [selectedCategoriesIncomes, setSelectedCategoriesIncomes] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchTransactions = async () => {
+            const authToken = localStorage.getItem('authToken');
+            const userId = localStorage.getItem('userId');
+            if (!authToken) {
+                navigate('/login');
+                return;
+            }
+
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/api/get_transactions/${userId}/`, {
+                    headers: { 'Authorization': `Bearer ${authToken}` }
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    const expenseTransactions = data.filter(t => t.type === 1);
+                    const incomeTransactions = data.filter(t => t.type === 0);
+                    setTransactions(expenseTransactions);
+                    setIncomes(incomeTransactions);
+                } else {
+                    console.error('Failed to fetch transactions');
+                }
+            } catch (error) {
+                console.error('Error fetching transactions:', error);
+            }
+        };
+
+        const fetchCategories = async () => {
+            const userId = localStorage.getItem('userId');
+            fetch(`http://127.0.0.1:8000/api/get_categories/${userId}/`)
+            .then((response) => {
+                if (!response.ok) { throw new Error("Error fetching user categories"); }
+                return response.json();
+            })
+            .then((data) => { setCategories(data); })
+            .catch((error) => {
+                console.error("Error fetching user categories:", error)
+            });
+        };
+        fetchTransactions();
+        fetchCategories();
+    }, [navigate]);
+
+    const handleFilter = async () => {
+        if (startDate && endDate) {
+            const start = new Date(startDate);
+            const end = new Date(endDate);
+
+            if (start > end) {
+                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
+                return;
+            }
+        }
+        if (minAmount && maxAmount) {
+            if (parseFloat(minAmount) > parseFloat(maxAmount)) {
+                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
+                return;
+            }
+        }
+        const params = new URLSearchParams();
+        params.append('type', 'expenses');
+        if (startDate) params.append('start_date', startDate);
+        if (endDate) params.append('end_date', endDate);
+        if (minAmount) params.append('min_amount', minAmount);
+        if (maxAmount) params.append('max_amount', maxAmount);
+        if (selectedCategories.length > 0) {
+            selectedCategories.forEach(category => params.append('categories', category));
+        }
+        const authToken = localStorage.getItem('authToken');
+        const userId = localStorage.getItem('userId');
+        try {
+            const response = await fetch(
+                `http://127.0.0.1:8000/api/filter_transactions/${userId}/?${params.toString()}`,
+                {
+                    headers: { 'Authorization': `Bearer ${authToken}` }
+                }
+            );
+
+            if (response.ok) {
+                const data = await response.json();
+                const expenseTransactions = data.filter(t => t.type === 1);
+                setTransactions(expenseTransactions);
+            } else {
+                console.error('Failed to filter transactions');
+            }
+        } catch (error) {
+            console.error('Error fetching filtered transactions:', error);
+        }
+    };
+
+    const handleIncomeFilter = async () => {
+        if (startDateIncomes && endDateIncomes) {
+            const start = new Date(startDateIncomes);
+            const end = new Date(endDateIncomes);
+            if (start > end) {
+                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
+                return;
+            }
+        }
+        if (minAmountIncomes && maxAmountIncomes) {
+            if (parseFloat(minAmountIncomes) > parseFloat(maxAmountIncomes)) {
+                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
+                return;
+            }
+        }
+        const params = new URLSearchParams();
+        params.append('type', 'incomes');
+        if (startDateIncomes) params.append('start_date', startDateIncomes);
+        if (endDateIncomes) params.append('end_date', endDateIncomes);
+        if (minAmountIncomes) params.append('min_amount', minAmountIncomes);
+        if (maxAmountIncomes) params.append('max_amount', maxAmountIncomes);
+        if (selectedCategoriesIncomes.length > 0) {
+            selectedCategoriesIncomes.forEach(category => params.append('categories', category));
+        }
+        const authToken = localStorage.getItem('authToken');
+        const userId = localStorage.getItem('userId');
+        try {
+            const response = await fetch(
+                `http://127.0.0.1:8000/api/filter_transactions/${userId}/?${params.toString()}`,
+                {
+                    headers: { 'Authorization': `Bearer ${authToken}` }
+                }
+            );
+            if (response.ok) {
+                const data = await response.json();
+                const incomeTransactions = data.filter(t => t.type === 0);
+                setIncomes(incomeTransactions);
+            } else {
+                console.error('Failed to filter transactions');
+            }
+        } catch (error) {
+            console.error('Error fetching filtered transactions:', error);
+        }
+    };
+
+    const handleCategoryChange = (event) => {
+        const selectedOptions = Array.from(event.target.selectedOptions, option => option.value);
+        setSelectedCategories(selectedOptions);
+    };
+
+    const handleIncomeCategoryChange = (event) => {
+        const selectedOptions = Array.from(event.target.selectedOptions, option => option.value);
+        setSelectedCategoriesIncomes(selectedOptions)
+    }
+
+
+    return(
+        <HistoryComponents
+            isCategoryDialogOpen={isCategoryDialogOpen}
+            setIsCategoryDialogOpen={setIsCategoryDialogOpen}
+            transactions={transactions}
+            categories={categories}
+            startDate={startDate}
+            setStartDate={setStartDate}
+            endDate={endDate}
+            setEndDate={setEndDate}
+            minAmount={minAmount}
+            setMinAmount={setMinAmount}
+            maxAmount={maxAmount}
+            setMaxAmount={setMaxAmount}
+            selectedCategories={selectedCategories}
+            isCategoryIncomeDialogOpen={isCategoryIncomeDialogOpen}
+            setIsCategoryIncomeDialogOpen={setIsCategoryIncomeDialogOpen}
+            incomes={incomes}
+            startDateIncomes={startDateIncomes}
+            setStartDateIncomes={setStartDateIncomes}
+            endDateIncomes={endDateIncomes}
+            setEndDateIncomes={setEndDateIncomes}
+            minAmountIncomes={minAmountIncomes}
+            setMinAmountIncomes={setMinAmountIncomes}
+            maxAmountIncomes={maxAmountIncomes}
+            setMaxAmountIncomes={setMaxAmountIncomes}
+            selectedCategoriesIncomes={selectedCategoriesIncomes}
+            setSelectedCategoriesIncomes={setSelectedCategoriesIncomes}
+            handleFilter={handleFilter}
+            handleCategoryChange={handleCategoryChange}
+            handleIncomeFilter={handleIncomeFilter}
+            handleIncomeCategoryChange={handleIncomeCategoryChange}
+        />
+    );
+};
+
+export default History;
--- frontend/src/pages/History/HistoryComponents.jsx
+++ frontend/src/pages/History/HistoryComponents.jsx
@@ -1,182 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import 'chartjs-adapter-date-fns';
 import Layout from '../../components/Layout/Layout.js';
 import './History.css';
-const History = () => {
-    const [isCategoryDialogOpen, setIsCategoryDialogOpen] = useState(false);
-    const [transactions, setTransactions] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [minAmount, setMinAmount] = useState('');
-    const [maxAmount, setMaxAmount] = useState('');
-    const [selectedCategories, setSelectedCategories] = useState([]);
 
-    const [isCategoryIncomeDialogOpen, setIsCategoryIncomeDialogOpen] = useState(false);
-    const [incomes, setIncomes] = useState([]);
-    const [startDateIncomes, setStartDateIncomes] = useState('');
-    const [endDateIncomes, setEndDateIncomes] = useState('');
-    const [minAmountIncomes, setMinAmountIncomes] = useState('');
-    const [maxAmountIncomes, setMaxAmountIncomes] = useState('');
-    const [selectedCategoriesIncomes, setSelectedCategoriesIncomes] = useState([]);
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const fetchTransactions = async () => {
-            const authToken = localStorage.getItem('authToken');
-            const userId = localStorage.getItem('userId');
-            if (!authToken) {
-                navigate('/login');
-                return;
-            }
-
-            try {
-                const response = await fetch(`http://127.0.0.1:8000/api/get_transactions/${userId}/`, {
-                    headers: { 'Authorization': `Bearer ${authToken}` }
-                });
-
-                if (response.ok) {
-                    const data = await response.json();
-                    const expenseTransactions = data.filter(t => t.type === 1);
-                    const incomeTransactions = data.filter(t => t.type === 0);
-                    setTransactions(expenseTransactions);
-                    setIncomes(incomeTransactions);
-                } else {
-                    console.error('Failed to fetch transactions');
-                }
-            } catch (error) {
-                console.error('Error fetching transactions:', error);
-            }
-        };
-        const fetchCategories = async () => {
-            const userId = localStorage.getItem('userId');
-            fetch(`http://127.0.0.1:8000/api/get_categories/${userId}/`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Error fetching user categories");
-                }
-                return response.json();
-            })
-            .then((data) => {
-                setCategories(data);
-            })
-            .catch((error) => {
-                console.error("Error fetching user categories:", error)
-            });
-        };
-        fetchTransactions();
-        fetchCategories();
-    }, [navigate]);
-
-    const handleFilter = async () => {
-        if (startDate && endDate) {
-            const start = new Date(startDate);
-            const end = new Date(endDate);
-
-            if (start > end) {
-                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
-                return;
-            }
-        }
-        if (minAmount && maxAmount) {
-            if (parseFloat(minAmount) > parseFloat(maxAmount)) {
-                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
-                return;
-            }
-        }
-        const params = new URLSearchParams();
-        params.append('type', 'expenses');
-        if (startDate) params.append('start_date', startDate);
-        if (endDate) params.append('end_date', endDate);
-        if (minAmount) params.append('min_amount', minAmount);
-        if (maxAmount) params.append('max_amount', maxAmount);
-        if (selectedCategories.length > 0) {
-            selectedCategories.forEach(category => params.append('categories', category));
-        }
-        const authToken = localStorage.getItem('authToken');
-        const userId = localStorage.getItem('userId');
-        try {
-            const response = await fetch(
-                `http://127.0.0.1:8000/api/filter_transactions/${userId}/?${params.toString()}`,
-                {
-                    headers: { 'Authorization': `Bearer ${authToken}` }
-                }
-            );
-
-            if (response.ok) {
-                const data = await response.json();
-                const expenseTransactions = data.filter(t => t.type === 1);
-                setTransactions(expenseTransactions);
-            } else {
-                console.error('Failed to filter transactions');
-            }
-        } catch (error) {
-            console.error('Error fetching filtered transactions:', error);
-        }
-    };
-
-    const handleIncomeFilter = async () => {
-        if (startDateIncomes && endDateIncomes) {
-            const start = new Date(startDateIncomes);
-            const end = new Date(endDateIncomes);
-            if (start > end) {
-                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
-                return;
-            }
-        }
-        if (minAmountIncomes && maxAmountIncomes) {
-            if (parseFloat(minAmountIncomes) > parseFloat(maxAmountIncomes)) {
-                alert("La fecha de inicio no puede ser posterior a la fecha de fin.");
-                return;
-            }
-        }
-        const params = new URLSearchParams();
-        params.append('type', 'incomes');
-        if (startDateIncomes) params.append('start_date', startDateIncomes);
-        if (endDateIncomes) params.append('end_date', endDateIncomes);
-        if (minAmountIncomes) params.append('min_amount', minAmountIncomes);
-        if (maxAmountIncomes) params.append('max_amount', maxAmountIncomes);
-        if (selectedCategoriesIncomes.length > 0) {
-            selectedCategoriesIncomes.forEach(category => params.append('categories', category));
-        }
-        const authToken = localStorage.getItem('authToken');
-        const userId = localStorage.getItem('userId');
-        try {
-            const response = await fetch(
-                `http://127.0.0.1:8000/api/filter_transactions/${userId}/?${params.toString()}`,
-                {
-                    headers: { 'Authorization': `Bearer ${authToken}` }
-                }
-            );
-            if (response.ok) {
-                const data = await response.json();
-                const incomeTransactions = data.filter(t => t.type === 0);
-                setIncomes(incomeTransactions);
-            } else {
-                console.error('Failed to filter transactions');
-            }
-        } catch (error) {
-            console.error('Error fetching filtered transactions:', error);
-        }
-    };
-
-    const handleCategoryChange = (event) => {
-        const selectedOptions = Array.from(event.target.selectedOptions, option => option.value);
-        setSelectedCategories(selectedOptions);
-    };
-
-    const handleIncomeCategoryChange = (event) => {
-        const selectedOptions = Array.from(event.target.selectedOptions, option => option.value);
-        setSelectedCategoriesIncomes(selectedOptions)
-    }
-
-
+const HistoryComponents = ({
+    isCategoryDialogOpen, setIsCategoryDialogOpen,
+    transactions,
+    categories,
+    startDate, setStartDate,
+    endDate, setEndDate,
+    minAmount, setMinAmount,
+    maxAmount, setMaxAmount,
+    selectedCategories,
+    isCategoryIncomeDialogOpen, setIsCategoryIncomeDialogOpen,
+    incomes,
+    startDateIncomes, setStartDateIncomes,
+    endDateIncomes, setEndDateIncomes,
+    minAmountIncomes, setMinAmountIncomes,
+    maxAmountIncomes, setMaxAmountIncomes,
+    selectedCategoriesIncomes,
+    handleFilter,
+    handleCategoryChange,
+    handleIncomeFilter,
+    handleIncomeCategoryChange,
+}) => {
     return (
         <Layout>
             <div className="expenses-page">
-
                 <h1 className="type-title">Expenses</h1>
                 <div className="add-expense-form">
                     <label htmlFor="start-date">Start Date</label>
@@ -185,7 +35,6 @@ const History = () => {
                         value={startDate}
                         onChange={(e) => setStartDate(e.target.value)}
                     />
-
                     <label htmlFor="end-date">End Date</label>
                     <input
                         type="date"
@@ -214,7 +63,6 @@ const History = () => {
                     >
                         Select Categories
                     </button>
-
                     <button onClick={handleFilter}>Filter</button>
                 </div>
 
@@ -231,8 +79,8 @@ const History = () => {
                             </thead>
                             <tbody>
                                 {transactions.map(transaction => {
-                                    console.log(transaction);  // Esto está bien para debuguear
-                                    return (  // Aquí debe ir el `return` para devolver el JSX
+                                    console.log(transaction);
+                                    return (
                                         <tr key={transaction.id_transaction}>
                                             <td>
                                                 {transaction.categories.map((categoryId, index) => {
@@ -253,11 +101,8 @@ const History = () => {
                                 })}
                             </tbody>
                         </table>
-
                     </div>
-
                 </div>
-
                 {isCategoryDialogOpen && (
                     <dialog className="category-dialog" open>
                         <h3>Select Categories</h3>
@@ -370,4 +215,5 @@ const History = () => {
         </Layout>
     );
 };
-export default History;
+
+export default HistoryComponents;
