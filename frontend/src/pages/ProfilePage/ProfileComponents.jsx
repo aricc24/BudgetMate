1cbce3b775e20ce1bec30cf7c6a1e93870fcf9aa
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-12-12 00:03:56 -0600

    style: Profile Page is done

--- frontend/src/pages/ProfilePage/ProfileComponents.jsx
+++ frontend/src/pages/ProfilePage/ProfileComponents.jsx
@@ -11,6 +11,8 @@ const ProfileComponents = ({
     handleSave,
     handleCancel,
     handleExit,
+    showPassword,
+    setShowPassword,
 }) => {
     return (
         <Layout>
@@ -28,67 +30,104 @@ const ProfileComponents = ({
                     <h2>My Profile</h2>
                     <form>
                         <div className="form-group">
+                            <label htmlFor="first_name">Name</label>
                             <input
                                 name="first_name"
                                 value={profileData.first_name}
                                 onChange={handleChange}
                                 placeholder="Name"
+                                autocomplete="off"
                                 disabled={!isEditing}
+                                className={!isEditing ? 'greyed' : ''}
                             />
+                            <label htmlFor="last_name_father">Father's surname</label>
                             <input
                                 name="last_name_father"
                                 value={profileData.last_name_father}
                                 onChange={handleChange}
                                 placeholder="Father's surname"
+                                autocomplete="off"
                                 disabled={!isEditing}
+                                className={!isEditing ? 'greyed' : ''}
                             />
+                            <label htmlFor="last_name_mother">Mother's surname</label>
                             <input
                                 name="last_name_mother"
                                 value={profileData.last_name_mother}
                                 onChange={handleChange}
                                 placeholder="Mother's surname"
+                                autocomplete="off"
                                 disabled={!isEditing}
+                                className={!isEditing ? 'greyed' : ''}
                             />
+                            <label htmlFor="rfc">RFC (13 characters)</label>
                             <input
                                 name="rfc"
                                 value={profileData.rfc}
                                 onChange={handleChange}
                                 placeholder="RFC"
+                                autocomplete="off"
+                                maxLength="13"
                                 disabled={!isEditing}
+                                className={!isEditing ? 'greyed' : ''}
                             />
                         </div>
                         <div className="form-group">
+                            <label htmlFor="curp">CURP (18 characters)</label>
                             <input
                                 name="curp"
                                 value={profileData.curp}
                                 onChange={handleChange}
                                 placeholder="CURP"
+                                autocomplete="off"
+                                maxLength="18"
                                 disabled={!isEditing}
+                                className={!isEditing ? 'greyed' : ''}
                             />
-                            <input
-                                name="email"
-                                value={profileData.email}
-                                onChange={handleChange}
-                                placeholder="Email"
-                                disabled={!isEditing}
-                            />
-                            <input
-                                name="password"
-                                type="password"
-                                value={profileData.password}
-                                onChange={handleChange}
-                                placeholder="Password"
-                                disabled={!isEditing}
-                            />
+                            <label htmlFor="email">Email</label>
+                            <div className="input-container">
+                                <i className="fas fa-envelope icon"></i>
+                                <input
+                                    name="email"
+                                    value={profileData.email}
+                                    onChange={handleChange}
+                                    placeholder="Email"
+                                    autocomplete="off"
+                                    disabled={!isEditing}
+                                    className={!isEditing ? 'greyed' : ''}
+                                />
+                            </div>
+                            <label htmlFor="password">Password</label>
+                            <div className="input-container">
+                                <i className="fas fa-key icon"></i>
+                                <input
+                                    name="password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    value={profileData.password}
+                                    onChange={handleChange}
+                                    placeholder="Password"
+                                    autocomplete="off"
+                                    disabled={!isEditing}
+                                    className={!isEditing ? 'greyed' : ''}
+                                />
+                                <i
+                                    className={`fas ${showPassword ? 'fa-eye-slash' : 'fa-eye'} toggle-password`}
+                                    onClick={() => setShowPassword(!showPassword)}
+                                ></i>
+                            </div>
+                            <label htmlFor="phone_number">Phone Number (15 digits)</label>
                             <input
                                 type="number"
                                 min="0"
-                                onKeyDown={(e) => {if (['e', 'E', '+', '-'].includes(e.key)) {e.preventDefault();}}}
+                                maxLength="15"
+                                onKeyDown={(e) => {if (['e', 'E', '+', '-', '.'].includes(e.key)) {e.preventDefault();}}}
                                 name="phone_number"
                                 value={profileData.phone_number}
                                 onChange={handleChange}
                                 placeholder="Phone Number"
+                                autocomplete="off"
                                 disabled={!isEditing}
+                                className={!isEditing ? 'greyed' : ''}
                             />
                         </div>
                     </form>
